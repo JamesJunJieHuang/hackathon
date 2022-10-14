5095697a8e5f9bfef21e446172ce5b8009b3a6b7
Wvaviator <wvaviator@gmail.com>
2022-10-14 12:31:14 -0500

    add js functionality to popup

--- destroymode.js
+++ destroymode.js
@@ -1,17 +1,18 @@
 let destroymode = false;
+const hammerElement = document.querySelector('#toggle img');
 
 function exec() {
-    destroymode = !destroymode;
-    if (destroymode) {
-        console.log('destroymode ON');
-        chrome.runtime.sendMessage({'myPopupIsOpen': true})
-    }
-    else console.log('destroymode OFF');
+  destroymode = !destroymode;
+  if (destroymode) {
+    console.log('destroymode ON');
+    hammerElement.style.opacity = 0;
+    chrome.runtime.sendMessage({ myPopupIsOpen: true });
+  } else {
+    hammerElement.style.opacity = 1;
+    console.log('destroymode OFF');
+  }
+  setTimeout(() => window.close(), 500);
 }
 
 const toggle = document.querySelector('#toggle');
-toggle.addEventListener('click', exec)
-
-
-
-
+toggle.addEventListener('click', exec);
